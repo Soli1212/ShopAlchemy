fde5bc4d5a721395364c2e524f439782cbd46a4d
Soli1212 <soheilworkmail.12@gmail.com>
2025-07-21 15:35:56 +0330

    frist commit | add connection.py

---
+++ connection.py
@@ -0,0 +1,40 @@
+from os import getenv
+
+from dotenv import load_dotenv
+from sqlalchemy import text
+from sqlalchemy.ext.asyncio import (AsyncSession, async_sessionmaker,
+                                    create_async_engine)
+from sqlalchemy.orm import declarative_base
+
+load_dotenv()
+
+DATABASE_URL = getenv("DATABASE_URL")
+
+engine = create_async_engine(url=DATABASE_URL, echo=True)
+
+async_session = async_sessionmaker(
+    bind=engine, class_=AsyncSession, expire_on_commit=False
+)
+
+Base = declarative_base()
+
+
+async def init_db():
+    async with engine.begin() as conn:
+        await conn.run_sync(Base.metadata.create_all)
+
+
+async def get_db():
+    async with async_session() as session:
+        async with session.begin():
+            yield session
+
+
+async def check_connection() -> bool:
+    try:
+        async with async_session() as session:
+            result = await session.execute(text("SELECT 1"))
+            return result.scalar() == 1
+    except Exception as e:
+        print("\n\n----------- Connection failed! ERROR:", e)
+        return False
